3f3b20117c0d0c2b1cc0af77790efbdf68fc745c
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-23 11:38:09 -0400

    New rails app for RPBox

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RPBox::Application.config.secret_key_base = 'a7dd96ed75a02ef97486af78cab3a463d160da555fd8568208fad41419ff0a7b8e20fd038fe2f2080972ef6506a259a1c1c5d419721d8e322ca70e2a6d1573ee'
